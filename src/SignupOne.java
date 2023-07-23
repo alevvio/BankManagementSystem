d252535742eee6824c2a82d337192cb207964653
Anupam <anupamcr7@gmail.com>
2023-07-23 23:06:58 +0530

    commit

--- src/SignupOne.java
+++ src/SignupOne.java
@@ -6,9 +6,25 @@ public class SignupOne extends JFrame {
 
     SignupOne(){
 
+        setLayout(null);
+
         Random ran = new Random();
-        System.out.println(ran.nextLong());
-        JLabel formno = new JLabel("APPLICATION FORM NO. " + );
+        long random = Math.abs((ran.nextLong()%9000L)+1000L);
+
+        JLabel formno = new JLabel("APPLICATION FORM NO. " + random);
+        formno.setFont(new Font("Raleway", Font.BOLD, 38));
+        formno.setBounds(140, 20, 600, 40);
+        add(formno);
+
+        JLabel personalDetails = new JLabel("Page 1: Personal Details");
+        personalDetails.setFont(new Font("Raleway", Font.BOLD, 22));
+        personalDetails.setBounds(290, 80, 400, 30);
+        add(personalDetails);
+
+        JLabel name = new JLabel("Name:");
+        name.setFont(new Font("Raleway", Font.BOLD, 20));
+        name.setBounds(100, 140, 100, 30);
+        add(name);
 
         getContentPane().setBackground(Color.WHITE);
         setSize(850, 800);
