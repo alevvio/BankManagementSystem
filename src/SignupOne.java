7b310597f750bb4a6f5323a97c25ead08fe0925b
Anupam <anupamcr7@gmail.com>
2023-07-23 13:54:01 +0530

    commit

--- src/SignupOne.java
+++ src/SignupOne.java
@@ -1,10 +1,16 @@
 import java.util.*;
 import javax.swing.*;
+import java.awt.*;
 
 public class SignupOne extends JFrame {
 
     SignupOne(){
 
+        Random ran = new Random();
+        System.out.println(ran.nextLong());
+        JLabel formno = new JLabel("APPLICATION FORM NO. " + );
+
+        getContentPane().setBackground(Color.WHITE);
         setSize(850, 800);
         setLocation(350, 10);
         setVisible(true);
