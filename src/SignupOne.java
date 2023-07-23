7f89669b534519d7871ee0169ccf2ebd0eb16fc7
Anupam <anupamcr7@gmail.com>
2023-07-23 13:40:22 +0530

    commit

---
+++ src/SignupOne.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import javax.swing.*;
+
+public class SignupOne extends JFrame {
+
+    SignupOne(){
+
+        setSize(850, 800);
+        setLocation(350, 10);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SignupOne();
+    }    
+}
