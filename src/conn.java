ef7253fbde56a844f02133283a3865ea3861246a
Anupam <anupamcr7@gmail.com>
2023-09-02 18:40:55 +0530

    commit

--- src/conn.java
+++ src/conn.java
@@ -1,16 +1,29 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 
 public class conn {
     
-    Connection c;
-    Statement s;
-    public conn(){
+    public static void main(String[] args) throws Exception {
         try {
-            c=DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "AK4756lexpc!");
-            s=c.createStatement();
+            String url = "jdbc:mysql://localhost:3306/";
 
-        } catch(Exception e) {
-            System.out.println(e);
+            String databaseName = "shivam";
+            String userName = "root";
+            String password = "AK4756lexpc!";
+    
+            Connection connection = DriverManager.getConnection(url,userName, password);
+    
+            String sql = "CREATE DATABASE " + databaseName;
+    
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+            statement.close();
+            JOptionPane.showMessageDialog(null, databaseName + " Database has been created successfully", "System Message", JOptionPane.INFORMATION_MESSAGE);
+    
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
